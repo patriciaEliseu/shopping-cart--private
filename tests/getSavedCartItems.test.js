1e924b2a3738fab90c349804a74d9fe3f49453eb
Patricia Eliseu <patyeliseu4@gmail.com>
2022-04-04 22:27:28 -0300

    add requisitos 10 e 11

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,15 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Teste1 se, ao executar getSavedCartItems, o método localStorage.getItem é chamado;', () => {
+    getSavedCartItems();
+    //ele espera que o localStoragegetItem seja chamado;
+    //  it waits for the localStorageSetItem to be called;
+    expect(localStorage.getItem).toHaveBeenCalled();
+  });
+  it('Teste se, ao executar getSavedCartItems, o método localStorage.getItem é chamado com o "cartItems" como parâmetro.', () => {
+    getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems');
+  });
+  // fail('Teste vazio');
 });
--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -5,18 +5,14 @@ localStorageSimulator('setItem');
 
 describe('4 - Teste a função saveCartItems', () => {
   it('Teste1 se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado.', () => {
-    const traz1 = saveCartItems(<ol><li>Item</li></ol>);
-    /* Use .toBeDefined para verificar se uma variável não é "indefinida". 
-    Por exemplo, se você quiser verificar se uma 
-    função fetchNewFlavorIdea() retorna algo, você pode escrever: */
-    expect(saveCartItems(localStorage.setItem(traz1))).toBeDefined();
-   });
-   it('Teste2 se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâm, sendo o 1º "cartItems" e o 2º sendo o valor passado como argumento para saveCartItems.', () => {
-    // const traz2 = saveCartItems(<ol><li>Item</li></ol>);
-    /* Use .toBeDefined para verificar se uma variável não é "indefinida". 
-    Por exemplo, se você quiser verificar se uma 
-    função fetchNewFlavorIdea() retorna algo, você pode escrever: */
-    expect(saveCartItems(<ol><li>Item</li></ol>)).toBeDefined(localStorage.setItem);
-   });
+    saveCartItems('<ol><li>Item</li></ol>');
+    //ele espera que o localStorageSetItem seja chamado;
+    //  it waits for the localStorageSetItem to be called;
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+  it('Teste2 se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâm, sendo o 1º "cartItems" e o 2º sendo o valor passado como argumento para saveCartItems.', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>');
+  });
   // fail('Teste vazio');
 });
